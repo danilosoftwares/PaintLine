b153254b46dc1d8f401c63eebfed964434912ebd
Danilo M Souza <danilo.softwares@gmail.com>
2022-02-07 16:29:04 -0300

    clonando paint no c#

--- PaintLine/Form1.Designer.cs
+++ PaintLine/Form1.Designer.cs
@@ -28,25 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.boxImage = new System.Windows.Forms.Panel();
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.picSelectRectangleRound = new System.Windows.Forms.PictureBox();
+            this.picSelectCircle = new System.Windows.Forms.PictureBox();
+            this.picSelectRectangle = new System.Windows.Forms.PictureBox();
+            this.picSelectLine = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlColorSelected = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectRectangleRound)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectCircle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectRectangle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // boxImage
+            // 
+            this.boxImage.BackColor = System.Drawing.Color.White;
+            this.boxImage.Location = new System.Drawing.Point(6, 122);
+            this.boxImage.Name = "boxImage";
+            this.boxImage.Size = new System.Drawing.Size(874, 577);
+            this.boxImage.TabIndex = 1;
+            this.boxImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.boxImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.boxImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.Controls.Add(this.picSelectRectangleRound);
+            this.pnlTitle.Controls.Add(this.picSelectCircle);
+            this.pnlTitle.Controls.Add(this.picSelectRectangle);
+            this.pnlTitle.Controls.Add(this.picSelectLine);
+            this.pnlTitle.Controls.Add(this.pictureBox2);
+            this.pnlTitle.Controls.Add(this.pnlColorSelected);
+            this.pnlTitle.Controls.Add(this.pictureBox1);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(1459, 116);
+            this.pnlTitle.TabIndex = 8;
+            // 
+            // picSelectRectangleRound
+            // 
+            this.picSelectRectangleRound.Image = global::PaintLine.Properties.Resources.no_selected_rectangle_round;
+            this.picSelectRectangleRound.Location = new System.Drawing.Point(513, 32);
+            this.picSelectRectangleRound.Name = "picSelectRectangleRound";
+            this.picSelectRectangleRound.Size = new System.Drawing.Size(20, 20);
+            this.picSelectRectangleRound.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picSelectRectangleRound.TabIndex = 11;
+            this.picSelectRectangleRound.TabStop = false;
+            this.picSelectRectangleRound.Click += new System.EventHandler(this.picSelectRectangleRound_Click);
+            // 
+            // picSelectCircle
+            // 
+            this.picSelectCircle.Image = global::PaintLine.Properties.Resources.no_selected_circle;
+            this.picSelectCircle.Location = new System.Drawing.Point(475, 32);
+            this.picSelectCircle.Name = "picSelectCircle";
+            this.picSelectCircle.Size = new System.Drawing.Size(20, 20);
+            this.picSelectCircle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picSelectCircle.TabIndex = 12;
+            this.picSelectCircle.TabStop = false;
+            this.picSelectCircle.Click += new System.EventHandler(this.picSelectCircle_Click);
+            // 
+            // picSelectRectangle
+            // 
+            this.picSelectRectangle.Image = global::PaintLine.Properties.Resources.no_selected_rectangle;
+            this.picSelectRectangle.Location = new System.Drawing.Point(494, 32);
+            this.picSelectRectangle.Name = "picSelectRectangle";
+            this.picSelectRectangle.Size = new System.Drawing.Size(20, 20);
+            this.picSelectRectangle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picSelectRectangle.TabIndex = 11;
+            this.picSelectRectangle.TabStop = false;
+            this.picSelectRectangle.Click += new System.EventHandler(this.picSelectRectangle_Click);
+            // 
+            // picSelectLine
+            // 
+            this.picSelectLine.BackColor = System.Drawing.Color.Transparent;
+            this.picSelectLine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picSelectLine.Image = global::PaintLine.Properties.Resources.no_selected_line;
+            this.picSelectLine.Location = new System.Drawing.Point(434, 32);
+            this.picSelectLine.Name = "picSelectLine";
+            this.picSelectLine.Size = new System.Drawing.Size(20, 20);
+            this.picSelectLine.TabIndex = 10;
+            this.picSelectLine.TabStop = false;
+            this.picSelectLine.Click += new System.EventHandler(this.picSelectLine_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = global::PaintLine.Properties.Resources.title_paint_right;
+            this.pictureBox2.Location = new System.Drawing.Point(1239, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(220, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pnlColorSelected
+            // 
+            this.pnlColorSelected.Location = new System.Drawing.Point(787, 33);
+            this.pnlColorSelected.Name = "pnlColorSelected";
+            this.pnlColorSelected.Size = new System.Drawing.Size(28, 28);
+            this.pnlColorSelected.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::PaintLine.Properties.Resources.title_paint2;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1250, 116);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::PaintLine.Properties.Resources.back_paint;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1459, 746);
+            this.Controls.Add(this.pnlTitle);
+            this.Controls.Add(this.boxImage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectRectangleRound)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectCircle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectRectangle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelectLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel boxImage;
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Panel pnlColorSelected;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picSelectLine;
+        private System.Windows.Forms.PictureBox picSelectRectangle;
+        private System.Windows.Forms.PictureBox picSelectCircle;
+        private System.Windows.Forms.PictureBox picSelectRectangleRound;
     }
 }
 
--- PaintLine/Form1.cs
+++ PaintLine/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,62 +11,237 @@ using System.Windows.Forms;
 
 namespace PaintLine
 {
+    enum ImageDrawing
+    {
+        Line,
+        Circle,
+        Rectangle,
+        RectangleArround,
+    }
+
+    struct HistoryImage
+    {
+        public int X;
+        public int Y;
+        public ImageDrawing Tipo;
+        public Color Cor;
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            pnlColorSelected.BackColor = selected;
+
+            MakePaletteColors();
         }
 
-        List<Point[]> pontos = new List<Point[]>();
+        Color selected = Color.Black;
+        ImageDrawing tipo = ImageDrawing.Line;
+        List<HistoryImage[]> pontos = new List<HistoryImage[]>();
         bool drw;
         int beginX, beginY;
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            drw = true;
-            beginX = e.X;
-            beginY = e.Y;
-            Point[] p = { new Point(e.X, e.Y), new Point() };
-            pontos.Add(p);
-        }
 
         Graphics g;
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
-        {
-            drw = false;
 
+        private void MakePaletteColors()
+        {
+            List<Color> palette = new List<Color>();
+            palette.Add(Color.Black);
+            palette.Add(Color.FromArgb(127, 127, 127));
+            palette.Add(Color.FromArgb(136, 0, 21));
+            palette.Add(Color.FromArgb(237, 28, 36));
+            palette.Add(Color.FromArgb(255, 127, 39));
+            palette.Add(Color.FromArgb(255, 242, 0));
+            palette.Add(Color.FromArgb(34, 177, 76));
+            palette.Add(Color.FromArgb(0, 162, 232));
+            palette.Add(Color.FromArgb(63, 72, 204));
+            palette.Add(Color.FromArgb(163, 73, 164));
+            palette.Add(Color.White);
+            palette.Add(Color.FromArgb(195, 195, 195));
+            palette.Add(Color.FromArgb(185, 122, 87));
+            palette.Add(Color.FromArgb(255, 174, 201));
+            palette.Add(Color.FromArgb(255, 201, 14));
+            palette.Add(Color.FromArgb(239, 228, 176));
+            palette.Add(Color.FromArgb(181, 230, 29));
+            palette.Add(Color.FromArgb(153, 217, 234));
+            palette.Add(Color.FromArgb(112, 146, 190));
+            palette.Add(Color.FromArgb(200, 191, 231));
+            int p = 0;
+            int y = 0;
+            while(y != 2)
+            { 
+                int x = 0;
+                while (x != 10)
+                {
+                    pnlTitle.Controls.Add(new Panel());
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).Location = new System.Drawing.Point(867 + (22 * x), 31 + (22 * y));
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).Name = "panel_" + this.Controls.Count.ToString();
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).Size = new System.Drawing.Size(16, 16);
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).BackColor = p < palette.Count ? palette[p] : Color.Aqua;
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).Click += new System.EventHandler(this.setColorPen_Click);
+                    (pnlTitle.Controls[pnlTitle.Controls.Count - 1] as Panel).BringToFront();
+                    x++;
+                    p++;
+                }
+                y++;
+            }
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void setColorPen_Click(object sender, EventArgs e)
         {
+            selected = (sender as Panel).BackColor;
+            pnlColorSelected.BackColor = selected;
+        }
 
+        public static GraphicsPath RoundedRect(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            if (radius == 0)
+            {
+                path.AddRectangle(bounds);
+                return path;
+            }
+
+            // top left arc  
+            path.AddArc(arc, 180, 90);
+
+            // top right arc  
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc  
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc 
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+        private void processImage(Color color,HistoryImage pt1, HistoryImage pt2)
+        {
+            Pen p = new Pen(color, 4);
+            if (pt1.Tipo == ImageDrawing.Line)
+            {
+                g.DrawLine(p, new Point(pt1.X, pt1.Y), new Point(pt2.X, pt2.Y));
+            }
+            else if (pt1.Tipo == ImageDrawing.Circle)
+            {
+                g.DrawEllipse(p, new Rectangle(pt1.X, pt1.Y, pt2.X - pt1.X, pt2.Y - pt1.Y));
+            }
+            else if (pt1.Tipo == ImageDrawing.Rectangle)
+            {
+                g.DrawRectangle(p, new Rectangle(pt1.X, pt1.Y, pt2.X - pt1.X, pt2.Y - pt1.Y));
+            }
+            else if (pt1.Tipo == ImageDrawing.RectangleArround)
+            {
+                g.DrawPath(p, RoundedRect(new Rectangle(pt1.X, pt1.Y, pt2.X - pt1.X, pt2.Y - pt1.Y), 5));
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "exemplo de linhas ponto a ponto";
-            this.BackColor = Color.White;
-            g = this.CreateGraphics();
+            this.Text = "exemplo de paint em c#";
+            //this.BackColor = Color.White;
+            //g = this.CreateGraphics();
+            g = boxImage.CreateGraphics();
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
             if (drw == true)
             {
-                g.Clear(Color.White);
-                Pen p = new Pen(Color.Blue, 4);
-                Point point1 = new Point(beginX, beginY);
-                Point point2 = new Point(e.X, e.Y);
-                g.DrawLine(p, point1, point2);
-                pontos[pontos.Count - 1][1] = new Point(e.X, e.Y);
+                g.Clear(Color.White);            
+                HistoryImage point1 = new HistoryImage();
+                point1.Tipo = tipo;
+                point1.Cor = selected;
+                point1.X = beginX;
+                point1.Y = beginY;
+                HistoryImage point2 = new HistoryImage();
+                point2.X = e.X;
+                point2.Y = e.Y;
+                processImage(point1.Cor, point1, point2);
+
+                pontos[pontos.Count - 1][1] = new HistoryImage();
+                pontos[pontos.Count - 1][1].X = e.X;
+                pontos[pontos.Count - 1][1].Y = e.Y;
                 int i = 0;
-                while(i != pontos.Count)
+                while (i != pontos.Count)
                 {
-                    g.DrawLine(p, pontos[i][0], pontos[i][1]);
+                    processImage(pontos[i][0].Cor, pontos[i][0], pontos[i][1]);
                     i++;
                 }
-   
             }
         }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drw = false;
+        }
+
+        private void picSelectLine_Click(object sender, EventArgs e)
+        {
+            tipo = ImageDrawing.Line;
+            processImageSelected();
+        }
+
+        private void picSelectCircle_Click(object sender, EventArgs e)
+        {
+            tipo = ImageDrawing.Circle;
+            processImageSelected();
+        }
+
+        private void picSelectRectangle_Click(object sender, EventArgs e)
+        {
+            tipo = ImageDrawing.Rectangle;
+            processImageSelected();
+        }
+
+        private void picSelectRectangleRound_Click(object sender, EventArgs e)
+        {
+            tipo = ImageDrawing.RectangleArround;
+            processImageSelected();
+        }
+
+        private void processImageSelected()
+        {
+            picSelectLine.Image = tipo == ImageDrawing.Line ? global::PaintLine.Properties.Resources.selected_line : global::PaintLine.Properties.Resources.no_selected_line;
+            picSelectCircle.Image = tipo == ImageDrawing.Circle ? global::PaintLine.Properties.Resources.selected_circle : global::PaintLine.Properties.Resources.no_selected_circle;
+            picSelectRectangle.Image = tipo == ImageDrawing.Rectangle ? global::PaintLine.Properties.Resources.selected_rectangle : global::PaintLine.Properties.Resources.no_selected_rectangle;
+            picSelectRectangleRound.Image = tipo == ImageDrawing.RectangleArround ? global::PaintLine.Properties.Resources.selected_rectangle_round : global::PaintLine.Properties.Resources.no_selected_rectangle_round;
+
+
+            switch (tipo)
+            {
+                case ImageDrawing.Line: picSelectLine.BringToFront(); break;
+                case ImageDrawing.Circle: picSelectCircle.BringToFront(); break;
+                case ImageDrawing.Rectangle: picSelectRectangle.BringToFront(); break;
+                case ImageDrawing.RectangleArround: picSelectRectangleRound.BringToFront(); break;
+            }
+        }
+
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drw = true;
+            beginX = e.X;
+            beginY = e.Y;
+            HistoryImage[] p = { new HistoryImage(), new HistoryImage() };
+            p[0].X = e.X;
+            p[0].Y = e.Y;
+            p[0].Tipo = tipo;
+            p[0].Cor = selected;
+            pontos.Add(p);
+        }
+        
     }
 }
